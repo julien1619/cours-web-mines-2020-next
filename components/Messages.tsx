22ce1671471bed52f9e7aa2eafacb9f5cb690050
Julien Blatecky <julien.blatecky@hey.com>
2020-12-01 18:07:12 +0100

    Move messages fetcher to Messages

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,15 +1,45 @@
+import useSWR from "swr";
+import ky from "ky/umd";
 import { MessageData } from "../types/messages";
 import Message from "./Message";
 
-type Props = {
-  messages: MessageData[];
-};
+async function fetchMessages() {
+  let pageIndex = 0;
+  let hasMessages = true;
+  let messages: MessageData[] = [];
 
-export default function Messages({ messages }: Props) {
+  while (hasMessages) {
+    // GET https://ensmn.herokuapp.com/messages
+    // eslint-disable-next-line
+    const pageMessages: MessageData[] = await ky
+      .get(`https://ensmn.herokuapp.com/messages?page=${pageIndex}`)
+      .json();
+
+    hasMessages = pageMessages.length > 0;
+    pageIndex += 1;
+    messages = messages.concat(pageMessages);
+  }
+
+  return { messages: messages.reverse() };
+}
+
+export default function Messages() {
+  const { data, error } = useSWR("/messages", fetchMessages, {
+    refreshInterval: 1000,
+  });
+
+  if (error != null) {
+    return <div>{error}</div>;
+  }
+
+  if (data == null) {
+    return <div>Chargement en cours...</div>;
+  }
+
   return (
     <div className="wrapper">
       <div className="messages-container">
-        {messages.map((message) => (
+        {data.messages.map((message) => (
           <Message key={message.timestamp} message={message} />
         ))}
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,9 @@
 import Head from "next/head";
-import useSWR from "swr";
-import ky from "ky/umd";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { MessageData } from "../types/messages";
 import Messages from "../components/Messages";
 import MessageForm from "../components/MessageForm";
 
-async function fetchMessages() {
-  let pageIndex = 0;
-  let hasMessages = true;
-  let messages: MessageData[] = [];
-
-  while (hasMessages) {
-    // GET https://ensmn.herokuapp.com/messages
-    // eslint-disable-next-line
-    const pageMessages: MessageData[] = await ky
-      .get(`https://ensmn.herokuapp.com/messages?page=${pageIndex}`)
-      .json();
-
-    hasMessages = pageMessages.length > 0;
-    pageIndex += 1;
-    messages = messages.concat(pageMessages);
-  }
-
-  return { messages: messages.reverse() };
-}
-
 export default function Home() {
-  const { data, error } = useSWR("/messages", fetchMessages, {
-    refreshInterval: 1000,
-  });
-
-  if (error != null) {
-    return <div>{error}</div>;
-  }
-
-  if (data == null) {
-    return <div>Chargement en cours...</div>;
-  }
-
   return (
     <>
       <Head>
@@ -50,7 +15,7 @@ export default function Home() {
       </nav>
       <div className="container-fluid">
         <div className="container">
-          <Messages messages={data.messages} />
+          <Messages />
         </div>
 
         <MessageForm />
